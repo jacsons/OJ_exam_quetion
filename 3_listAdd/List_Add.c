423bff4f406df437c9dd9a27e0d9f9042a59cc74
jacsons <yeyh0728@gmail.com>
2019-12-13 23:09:00 +0800

    leecode: list add : two list number sume

--- 3_listAdd/List_Add.c
+++ 3_listAdd/List_Add.c
@@ -1,3 +1,25 @@
+// #### 题目描述
+/*
+给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+
+如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+
+您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+示例：
+
+输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+输出：7 -> 0 -> 8
+原因：342 + 465 = 807
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/add-two-numbers
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+// #### 本题答案
+// https://leetcode-cn.com/problems/add-two-numbers/
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -6,70 +28,123 @@
  * };
  */
 
-struct ListNode {
-    int val;
-    struct ListNode *next;
-};
+// 两数相加
+// 链表异常相加，会出现三种情况
 
-struct ListNode processDoubleLinkAdd(struct ListNode *head, struct ListNode* l1, struct ListNode* l2)
+// 1、没有进位，长度为传入长度
+// 2、刚刚好有一个进位，那多申请一个长度在尾部
+// 3、两个长度差距很大，这进位后 +0；
+
+// 函数设计
+// 传入数据 两个链表
+// 1、两者先相加
+// 2、判断最后一个是否大于10，大于记录，进行尾部处理
+// 3、返回
+
+
+/*
+*  申请新的node
+*/
+struct ListNode* mallocListNode()
 {
-    struct ListNode *first, *next;
-    struct ListNode *resultListNode = NULL;
+    struct ListNode* result = NULL;
 
-     // init
-    head = resultListNode;
-    first = l1;
-    next = l2;
+    result = (struct ListNode*)malloc(sizeof(struct ListNode));
+    if (result == NULL) {
+        return NULL;
+    }
 
-    while (first != NULL && next != NULL)
-    {
-         resultListNode->val = first->val + next->val + pre;
-         pre = resultListNode->val / 10;
-         resultListNode->val = resultListNode->val % 10;
-        
-         printf("%d pre = %d", resultListNode->val, pre);
-         resultListNode->next = (struct ListNode *)malloc(sizeof(struct ListNode)); 
-         memset(resultListNode->next, 0x00, sizeof(struct ListNode));
-         resultListNode = resultListNode->next;
-    };
+    result = memset(result, 0, sizeof(struct ListNode));
+    if (result == NULL) {
+        return NULL;
+    }
+
+    return result;
+}
+
+struct ListNode* processTailValue(struct ListNode *resultListNode, int preValue)
+{
+    if (preValue > 0) {
+        resultListNode->next = mallocListNode();
+        resultListNode = resultListNode->next;
+        resultListNode->val = preValue; 
+    }
 
     return resultListNode;
 }
 
-
-void processTail(struct ListNode *resultListNode, struct ListNode* l1, int preValue)
+struct ListNode* processTail(struct ListNode *resultListNode, struct ListNode* inpuListNode, int preValue)
 {
-    struct ListNode *current = NULL;
-    current = l1;
-    do
+    struct ListNode *firstInputNext = NULL;
+    int twoNodeSum = preValue;
+   
+    firstInputNext = inpuListNode;
+    while (firstInputNext != NULL)
     {
-        resultListNode->val = current->val + preValue;
-        resultListNode->next = (struct ListNode*)malloc(sizeof(struct ListNode));
-        preValue = resultListNode->val / 10;
-        resultListNode->val = resultListNode->val % 10;
+        resultListNode->next = mallocListNode();
         resultListNode = resultListNode->next;
-        current = current->next;
-    } while (current->next != NULL);
+        twoNodeSum = twoNodeSum + firstInputNext->val;
+        resultListNode->val = twoNodeSum % 10; 
+        twoNodeSum = (twoNodeSum - resultListNode->val) / 10;
+        
+        firstInputNext = firstInputNext->next;
+    }
+    
+    processTailValue(resultListNode, twoNodeSum);
+
+    return resultListNode;
 }
 
+struct ListNode* processTwoLinkAdd(struct ListNode* l1, struct ListNode* l2)
+{
+    int twoNodeSum = 0;
+    struct ListNode *resultListNode = NULL;
+    struct ListNode *resultHead = NULL;
+    struct ListNode *firstInputNext = NULL;
+    struct ListNode *lastInputNext = NULL;
+
+    firstInputNext = l1;
+    lastInputNext = l2;
+
+    resultHead = mallocListNode();
+    resultListNode = resultHead;
+    while (firstInputNext != NULL && lastInputNext != NULL)
+    {
+       resultListNode->next = mallocListNode();
+       resultListNode = resultListNode->next;
+       twoNodeSum = twoNodeSum + firstInputNext->val + lastInputNext->val;
+       resultListNode->val = twoNodeSum % 10;
+       twoNodeSum = (twoNodeSum - resultListNode->val) / 10;
+
+       firstInputNext = firstInputNext->next;
+       lastInputNext = lastInputNext->next;
+    };
+
+    if (firstInputNext != NULL || lastInputNext != NULL) {
+       if (firstInputNext != NULL) {
+           processTail(resultListNode, firstInputNext, twoNodeSum);
+       } else {
+           processTail(resultListNode, lastInputNext, twoNodeSum);
+       } 
+    } else
+    {
+         processTailValue(resultListNode, twoNodeSum);
+    }
+    
+    // 去除头结点
+    resultListNode = resultHead->next;
+    free(resultHead);
+
+    return resultListNode;
+}
 
 // main function
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
 {
-    struct ListNode *head = NULL;
-    resultListNode = (struct ListNode *)malloc(sizeof(struct ListNode));
-
-   
-    
-    if (first->next == NULL && next->next != NULL) {
-        return head;
+    if (l1 == NULL && l2 == NULL) {
+        return NULL;
     }
-
-    if (first->next != NULL) {
-        processTail(resultListNode, first->next, pre);
-    } else {
-        processTail(resultListNode, next->next, pre);
-    }
-
-    return head;
+    // process two list add
+    return processTwoLinkAdd(l1, l2);
 }
+
